9cac2355bfe995986cd24624574b6505abf27f2d
Nurgissa Itkarinov <nurgissa.22@gmail.com>
2022-12-26 21:44:08 +0600

    week 7

---
+++ 7 week/linkedListExample.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class linkedListExample {
+
+    class Node {
+        int value;
+        Node next;
+    }
+
+    linkedListExample(int a, int b, int c) {
+
+        // a -> b -> c -> null
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> numbers = new LinkedList<Integer>();
+        numbers.add(5);
+        numbers.add(10);
+        numbers.add(15);
+        System.out.println(numbers);
+    }
+}
